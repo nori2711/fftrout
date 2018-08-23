43938e1b95f2a1d432a1948f7a579b83de7f15ec
nori <maruyama@maruyamahanwanoMacBook.local>
2018-08-23 18:09:50 +0900

    入渓ポイント表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   root to: "top#index"
   resources :hunts, only: [:index, :new, :create, :show]
   resources :top, only: [:show]
